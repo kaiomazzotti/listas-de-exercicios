eb88c1fd29a0e94fbb26274cae1c1a9dba4de84f
KaioMazzotti <kaio.mazzotti@gmail.com>
2022-12-14 15:22:28 -0300

    first release

---
+++ Date.Time/Date.Time/Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+         //DateTime data = new DateTime();
+         DateTime agora = DateTime.Now;
+         //DateTime minhaData = new DateTime(1980, 12, 27, 8, 0, 0);
+
+         Console.WriteLine(DateTime.Now.Year);
+         Console.WriteLine(agora.Month);
+         Console.WriteLine(agora.Day);
+         Console.WriteLine(agora.Hour);
+         Console.WriteLine(agora.Minute);
+         Console.WriteLine(agora.Second);
+         Console.WriteLine(agora.Millisecond);
+         Console.WriteLine(agora.DayOfWeek);
+         Console.WriteLine(agora.DayOfYear);
+         Console.WriteLine();
+         Console.WriteLine(agora.ToString()); //30/11/2022 19:53:45
+         Console.WriteLine(agora.ToLongDateString()); //quarta-feira, 30 de novembro de 2022
+         Console.WriteLine(agora.ToShortDateString()); //30/11/2022
+         Console.WriteLine(agora.ToLongTimeString()); //19:53:45
+         Console.WriteLine(agora.ToShortTimeString()); //19:53
+         string dataLonga = agora.ToLongDateString();  
+         */
+
+#region  DateTime ex 1
+//Console.Write("Digite Dia: ");
+//int dia = int.Parse(Console.ReadLine());
+//Console.Write("Digite Mês: ");
+//int mes = int.Parse(Console.ReadLine());
+//Console.Write("Digite Ano: ");
+//int ano = int.Parse(Console.ReadLine());
+//DateTime data = new DateTime(ano, mes, dia);
+
+//Console.WriteLine("Bom dia, hoje é dia " + data.ToString("dd") + " de " + data.ToString("MMMM") + " de " + data.ToString("yyyy"));
+#endregion
+
+#region  DateTime ex 2
+//Console.Write("Digite o Dia: ");
+//int dia = int.Parse(Console.ReadLine());
+//Console.Write("Digite o Mês: ");
+//int mes = int.Parse(Console.ReadLine());
+//Console.Write("Digite o Ano: ");
+//int ano = int.Parse(Console.ReadLine());
+//DateTime data = new DateTime(ano, mes, dia);
+//Console.WriteLine("Digite o numero de dias para acrescentar: ");
+//int diaadd = int.Parse(Console.ReadLine());
+//Console.WriteLine("Digite o numero de meses para acrescentar: ");
+//int mesadd = int.Parse(Console.ReadLine());
+//Console.WriteLine("Digite o numero de anos para acrescentar: ");
+//int anoadd = int.Parse(Console.ReadLine());
+
+//data = data.AddDays(diaadd);
+//data = data.AddMonths(mesadd);
+//data = data.AddYears(anoadd);
+
+//Console.WriteLine(data.ToShortDateString());
+#endregion
+
+#region DateTime ex 3
+//DateTime agora = DateTime.Now;
+//Console.WriteLine(agora.ToString("dddd"));
+#endregion
